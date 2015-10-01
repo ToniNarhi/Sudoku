f8d298640487e421192b0ac88c320df182a89f36
somename <someemail>
2015-10-01 13:26:46 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -34,7 +34,7 @@ public class SudokuVerifier {
 				if(x == firstrow[i][0])
 				{
 					System.out.println("Answer Array runs!");
-					answerArray[x]++;
+					answerArray[x-1]++;
 				}	
 			}
 		
