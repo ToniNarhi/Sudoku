3d10aebb33ad7487553b06d7b17d15f981554957
somename <someemail>
2015-10-01 13:21:51 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -34,7 +34,7 @@ public class SudokuVerifier {
 				if(x == firstrow[i][0])
 				{
 					System.out.println("Answer Array runs!");
-					answerArray[i] = answerArray[i] + 1;
+					answerArray[i] = firstrow[i][0];
 				}	
 			}
 		
