578e86dcfae91cf97109563fc40cb3d37884c02d
somename <someemail>
2015-10-01 12:23:50 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -6,12 +6,18 @@ public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
-		String[] firstrow = new String[10];
+		//String[] firstrow = new String[10];
 		String[] stringArray = new String[] {candidateSolution};
-		for(int i = 0; i < 8; i++)
+		int[][] firstrow = new int[stringArray.length/9][stringArray.length/9];
+		//String[] stringArray = new String[] {candidateSolution};
+		//int candidateSolInt = Integer.parseInt(candidateSolution);
+		for(int i = 0; i < stringArray.length/9; i++)
 		{
-		firstrow[0] = stringArray[0];
-		System.out.println(firstrow[0]);
+			for(int u = 0; u < stringArray.length/9; u++)
+			{
+				firstrow[i][u] = Integer.parseInt(candidateSolution);
+				System.out.println(firstrow[0]);
+			}
 		}	
 		System.out.println("Runs!");
 		return 0;
