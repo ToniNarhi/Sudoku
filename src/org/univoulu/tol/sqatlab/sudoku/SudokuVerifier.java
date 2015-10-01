eaf7e6bf7547ed7069a87ce6d0b056c149ca0dbb
somename <someemail>
2015-10-01 13:18:46 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -30,7 +30,7 @@ public class SudokuVerifier {
 				int x =0;
 				x= u +1;
 				System.out.println("U:" + x);
-				System.out.println("firstrow:" + firstrow[u][0]);
+				System.out.println("firstrow:" + firstrow[i][0]);
 				if(x == firstrow[u][0])
 				{
 					System.out.println("Answer Array runs!");
