4327b1079058c74cea1059f79d0ed98331b59245
somename <someemail>
2015-10-01 13:15:02 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -30,7 +30,7 @@ public class SudokuVerifier {
 				int x =0;
 				x= u +1;
 				System.out.println("U:" + x);
-				if(u+1 == firstrow[u][0])
+				if(x == firstrow[u][0])
 				{
 					
 					answerArray[i] = answerArray[i] + 1;
