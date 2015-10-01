8c84a9f064de99f94fb09a38b77be0afbd3b9f88
somename <someemail>
2015-10-01 12:13:28 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -6,7 +6,7 @@ public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
-		String[] firstrow = new String[9];
+		String[] firstrow = new String[10];
 		String[] stringArray = new String[] {candidateSolution};
 		for(int i = 0; i < 8; i++)
 		{
