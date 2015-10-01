0380bbebc8442d1af7e49750703e4d0a3953f69b
somename <someemail>
2015-10-01 11:48:33 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/Main.java
+++ src/org/univoulu/tol/sqatlab/sudoku/Main.java
@@ -1,5 +1,5 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
 public class Main {
-	SudokuVerifier();
+	SudokuVerifier sudokuVerifier = new SudokuVerifier(417369825632158947958724316825437169791586432346912758289643571573291684164875293);
 }
