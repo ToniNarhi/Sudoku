cb9990d53ae616ed2794210d3ce39d511a0c9550
somename <someemail>
2015-10-01 12:29:52 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -11,6 +11,7 @@ public class SudokuVerifier {
 		int[][] firstrow = new int[stringArray.length/9][stringArray.length/9];
 		//String[] stringArray = new String[] {candidateSolution};
 		//int candidateSolInt = Integer.parseInt(candidateSolution);
+		System.out.println(stringArray.length/9);
 		for(int i = 0; i < stringArray.length/9; i++)
 		{
 			System.out.println("Array i Runs!");
