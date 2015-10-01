ecf31269764cf3075e5984a0384dafa081499f4a
somename <someemail>
2015-10-01 13:25:09 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/Main.java
+++ src/org/univoulu/tol/sqatlab/sudoku/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-	String sudokuValues = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String sudokuValues = "411369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	SudokuVerifier sudokuVerifier = new SudokuVerifier();
 	sudokuVerifier.verify(sudokuValues);
 
